d3a8c9b45451cd204516135e4b93a78a08dd7a41
OwenKane <owen.a.kane@gmail.com>
2015-10-14 18:12:32 +0300

    work on lab5

--- Lab5/my_application/server.py
+++ Lab5/my_application/server.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("/hello")
+def i1();
+    return "Hello World"
+
+@app.route("/user/paul/")
+def i2();
+    return "User Paul"
+
+@app.route("/post/80")
+def i3
+    return "Post 80"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080,debug=True)
